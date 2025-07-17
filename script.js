6067935b5aff60c32f812eb58d5d8c41621298dc
kiwiofdoom1337 <186161940+kiwiofdoom1337@users.noreply.github.com>
2025-07-17 14:20:24 +0300

    make computer rps code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNum === 1) {
+    return "Rock";
+  } else if (randomNum === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
